369ce242bd0db96cea208a753a381b8bb567b55d
dan <chezydan@gmail.com>
2022-08-11 18:56:25 +0300

    Updates

---
+++ precache-manifest.dc7bf5aa469a03017e8222570b61ae56.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f0fa8b637af2cf4532f8",
+    "url": "/reactnotes0/static/css/main.4a7d28f0.chunk.css"
+  },
+  {
+    "revision": "f0fa8b637af2cf4532f8",
+    "url": "/reactnotes0/static/js/main.f0fa8b63.chunk.js"
+  },
+  {
+    "revision": "a673ba0059963469d7a4",
+    "url": "/reactnotes0/static/js/1.a673ba00.chunk.js"
+  },
+  {
+    "revision": "7e2dc7f41b6b80d72e8d",
+    "url": "/reactnotes0/static/js/runtime~main.7e2dc7f4.js"
+  },
+  {
+    "revision": "d9901b4e2dfe8085ccdadebfbcadecb4",
+    "url": "/reactnotes0/index.html"
+  }
+];
